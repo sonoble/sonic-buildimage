aaf28cc75aad62ae8cad6dd9d9fd6464bc5003b3
James Huang <huang.james@inventec.com>
2020-03-08 11:31:42 +0800

    To integrate SONiC 201911 with SAI 4.1.1.7 and SDK 6.5.17

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,9 +1,12 @@
-BRCM_SAI = libsaibcm_3.5.3.1m-25_amd64.deb
-$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/3.5/libsaibcm_3.5.3.1m-26_amd64.deb?sv=2015-04-05&sr=b&sig=zo83IKnlT7goymXwynW8%2Fx6rR2eIh0AiIS%2BSrSMUhRE%3D&se=2033-07-21T18%3A50%3A27Z&sp=r"
+BRCM_SAI_VERSION = 4.1.1.7-1
 
-BRCM_SAI_DEV = libsaibcm-dev_3.5.3.1m-25_amd64.deb
-$(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
-$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/3.5/libsaibcm-dev_3.5.3.1m-26_amd64.deb?sv=2015-04-05&sr=b&sig=tQmkCIy2mnb9rH7B9oXFUZDwijMGXWnVtta2CNTMbFM%3D&se=2033-07-21T18%3A50%3A47Z&sp=r"
+export BRCM_SAI_VERSION
 
-SONIC_ONLINE_DEBS += $(BRCM_SAI)
+BRCM_SAI = libsaibcm_$(BRCM_SAI_VERSION)_amd64.deb
+$(BRCM_SAI)_PATH = files/broadcom-binary
+
+BRCM_SAI_DEV = libsaibcm-dev_$(BRCM_SAI_VERSION)_amd64.deb
+$(BRCM_SAI_DEV)_PATH = files/broadcom-binary
+
+SONIC_COPY_DEBS += $(BRCM_SAI) $(BRCM_SAI_DEV)
 $(BRCM_SAI_DEV)_DEPENDS += $(BRCM_SAI)
